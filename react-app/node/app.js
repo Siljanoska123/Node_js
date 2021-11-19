85017aed5c4ef0cb56ad70195e36ce3958c640bf
Ivana Siljanoska <jancevska.ivana@gmail.com>
2021-11-19 14:08:30 +0100

    react and node app

---
+++ react-app/node/app.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors')
+const mongoose = require('mongoose');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const postsRouter = require('./routes/posts');
+const commentsRouter = require('./routes/comments');
+const paymentsRouter = require('./routes/payments');
+
+var app = express();
+
+require('dotenv').config();
+
+// mongoose.connect('mongodb://localhost:27017/gen-14-ws');
+// mongoose.connect(`mongodb+srv://bobz:${process.env.MONGODB_PASSWORD}@cluster0.ngbhd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
+mongoose.connect(`mongodb+srv://node-js:${process.env.MONGODB_PASSWORD}@cluster0.hd6bi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+`)
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
+app.use('/comments', commentsRouter);
+app.use('/payments', paymentsRouter);
+app.use(cors())
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).send({
+      error: true,
+      message: 'You need to log in to perform this action'
+    })
+  }
+});
+
+module.exports = app;
---
+++ react-app/node/routes/payments.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/payments');
+const jwt = require('express-jwt');
+
+require('dotenv').config();
+
+router.get('/', controller.all)
+router.use(jwt({ secret: process.env.AUTH_SECRET, algorithms: ['HS256']}));
+router.post('/create', controller.create)
+      .post('/authorize', controller.authorize)
+      .post('/:id/void', controller.void)
+      .post('/:id/capture', controller.capture)
+      .post('/:id/refund', controller.refund)
+
+module.exports = router;
